bcd826da76943d08d09863a9af31c4692d25d6af
RvonGlahn <rasmusvong@gmx.de>
2021-05-31 16:15:36 +0200

    fix: server.js without helmet

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 const port = process.env.PORT || 8080;
-const hostname = process.env.HOST;
+const hostname = process.env.HOST || "0.0.0.0";
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
